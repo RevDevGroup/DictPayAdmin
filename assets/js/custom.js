1bb51fa4870c88665af0d1086a655979fc78f66b
JimScope <jimscope@protonmail.com>
2020-12-12 22:26:50 -0500

    Dashboard Redesigned

---
+++ assets/js/custom.js
@@ -0,0 +1,9 @@
+if (halfmoon.getPreferredMode() == "light-mode") {
+    // Light mode is preferred
+    }
+else if (halfmoon.getPreferredMode() == "dark-mode") {
+    // Dark mode is preferred
+}
+else if (halfmoon.getPreferredMode() == "not-set") {
+    // Cookie does not exist
+}
